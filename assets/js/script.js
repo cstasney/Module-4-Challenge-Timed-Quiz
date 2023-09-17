2d2ba1876509a9d373286863885a57b58a5347d1
cstasney <cstasney07@gmail.com>
2023-09-17 12:30:16 -0400

    js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var button = document.querySelector(".start-button");
+var questions = "Which of the following is correct?";
+
+function startGame() {
+    console.log(window)
+}
+
+// Click button to start quiz function
+button.addEventListener('click', startGame);
