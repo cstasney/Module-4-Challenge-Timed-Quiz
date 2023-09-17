d376a8688119ee0d56e666a7a745b9db31a38bdf
cstasney <cstasney07@gmail.com>
2023-09-17 17:39:14 -0400

    killme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,39 @@
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
 var timerElement = document.querySelector(".timer-count");
-var button = document.querySelector(".start-button");
-var questions = "Which of the following is correct?";
+var startbutton = document.querySelector(".start-button");
+var correct = document.querySelector("correct-answers");
 
-function startGame() {
-    console.log(window)
+
+var questions = "Which of the following is correct?"
+
+// Initiate quiz function
+function startQuiz() {
+    startTimer();
+}
+
+
+// The setTimer function starts the timer
+function startTimer() {
+    startbutton.disabled = true;
+    var timeLeft = 3;
+    var timeInterval = setInterval(function () {
+
+        if (timeLeft > 1) {
+            timerElement.textContent = timeLeft + " Seconds Remaining";
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerElement.textContent = timeLeft = " 1 Second Remaining";
+        } else {
+            timerElement.textContent = "";
+            clearInterval(timeInterval)
+            endQuiz();
+        }
+    }, 1000);
+}
+
+// Function to end Quiz
+
+function endQuiz() {
 }
 
 // Click button to start quiz function
-button.addEventListener('click', startGame);
+startbutton.addEventListener('click', startQuiz);
